6acf2e437565828a500eda696b5fae5669a84d4d
Lucas Vilela Souza <souza.vilela.lucas@gmail.com>
2023-07-10 22:00:17 +0200

    add  detailed_tracks(db)

--- 03-AR-Database/01-DB-Design-and-SQL/05-SQL-queries/lib/join_queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/05-SQL-queries/lib/join_queries.rb
@@ -2,6 +2,12 @@ require 'sqlite3'
 
 def detailed_tracks(db)
   # TODO: return the list of tracks with their album and artist.
+  db.execute(%(
+    SELECT tracks.name, albums.title, artists.name
+    FROM tracks
+    INNER JOIN albums ON tracks.album_id = albums.id
+    INNER JOIN artists ON albums.artist_id = artists.id
+  ))
 end
 
 def stats_on(db, genre_name)
