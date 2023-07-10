9c39b35e024f4dc0932b4bfd845809d1456e8d4a
Lucas Vilela Souza <souza.vilela.lucas@gmail.com>
2023-07-10 22:00:34 +0200

    add stats_on(db, genre_name)

--- 03-AR-Database/01-DB-Design-and-SQL/05-SQL-queries/lib/join_queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/05-SQL-queries/lib/join_queries.rb
@@ -12,7 +12,12 @@ end
 
 def stats_on(db, genre_name)
   # TODO: For the given category of music, return the number of tracks and the average song length (as a stats hash)
-end
+  stat = db.execute(%(
+    SELECT genres.name, COUNT(tracks.id), ROUND(AVG(milliseconds) / 60000, 2)
+    FROM tracks
+    INNER JOIN genres ON genres.id = tracks.genre_id
+    WHERE genres.name = '#{genre_name}'
+  )).flatten
 
 def top_five_artists(db, genre_name)
   # TODO: return list of top 5 artists with the most songs for a given genre.
