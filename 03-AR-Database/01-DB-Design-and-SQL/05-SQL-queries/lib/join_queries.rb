4be86d1dab0bf1fae5794455020b694cbe69974d
Lucas Vilela Souza <souza.vilela.lucas@gmail.com>
2023-07-10 22:01:01 +0200

    add top_five_artists(db, genre_name)

--- 03-AR-Database/01-DB-Design-and-SQL/05-SQL-queries/lib/join_queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/05-SQL-queries/lib/join_queries.rb
@@ -19,6 +19,24 @@ def stats_on(db, genre_name)
     WHERE genres.name = '#{genre_name}'
   )).flatten
 
+  return {
+    category: stat[0],
+    number_of_songs: stat[1],
+    avg_length: stat[2]
+  }
+end
+
 def top_five_artists(db, genre_name)
   # TODO: return list of top 5 artists with the most songs for a given genre.
+  db.execute(%(
+    SELECT artists.id, artists.name, COUNT(tracks.id) AS track_count
+    FROM tracks
+    INNER JOIN albums ON tracks.album_id = albums.id
+    INNER JOIN artists ON albums.artist_id = artists.id
+    INNER JOIN genres ON tracks.genre_id = genres.id
+    WHERE genres.name = '#{genre_name}'
+    GROUP BY artists.id
+    ORDER BY track_count DESC
+    LIMIT 5
+  ))
 end
