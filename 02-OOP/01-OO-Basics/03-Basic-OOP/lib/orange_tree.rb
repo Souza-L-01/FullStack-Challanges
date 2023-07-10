aac19180e12e6bd3278bc87b3ba8477a2da9d3e3
Lucas Vilela Souza <souza.vilela.lucas@gmail.com>
2023-07-10 21:56:19 +0200

    revise logic OOP three exersice

--- 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
+++ 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
@@ -1,89 +1,51 @@
-# class OrangeTree
-#    attr_reader :age, :height, :fruit_count
-
-#   def initialize
-#     @age = 0
-#     @height = 0
-#     @fruits = 0
-#     @dead = false
-#   end
-#   # TODO: Implement all the specs defined in the README.md :)
-
-#   def one_year_passes!
-#     @age += 1
-#     @height +=1 unless @age >= 10
-
-#     if @age > 5 && @age <=10
-#       @fruit_count = 100
-#       elsif @age >= 10
-#         @fruit_count == 200
-#       else
-#         @fruit_count == 0
-#       end
-
-#       if @age >= 50
-#         @dead = rand(100) < 50
-#       end
-      
-#       if age  = 100
-#         @dead == true
-#       end
-
-#       @fruit_count = 0 if @dead
-#       end
-
-#       def @dead
-#         @dead
-#       end
-
-#       def pick_a_fruit!
-#         if fruit_count > 0
-#           fruit_count -= 1
-#         else
-#         end
-#     end
-#   end
-# end
-
-
-
 class OrangeTree
-  attr_reader :age, :height, :fruit_count
+  attr_reader :age, :height, :fruits
 
   def initialize
     @age = 0
     @height = 0
-    @fruit_count = 0
+    @fruits = 0
     @dead = false
-  end
-
-  def one_year_passes!
-    @age += 1
-    @height = age <= 10 ? age : 10
-    if age > 5 && age <= 10
-      @fruit_count = 100
-    elsif age > 10 && age <= 15
-      @fruit_count = 200
-    else
-      @fruit_count = 0
-    end
-    if age > 50
-      @dead = rand(100) < age - 50
-    end
-    if age >= 100
-      @dead = true
-    end
-    @fruit_count = 0 if @dead
   end
 
   def dead?
     @dead
   end
 
+  def one_year_passes!
+    unless dead?
+      may_die!
+      grow!
+      produce_fruits!
+    end
+  end
+
   def pick_a_fruit!
-    if @fruit_count > 0
-      @fruit_count -= 1
+    @fruits -= 1 if @fruits.positive?
+  end
+
+  private
+
+  def may_die!
+    if @age == 100
+      @dead = true
+    elsif @age > 50
+      @dead = (@age >= rand(51..100))
+    end
+  end
+
+  def grow!
+    @age += 1
+    @height += 1 if @age <= 10
+  end
+
+  def produce_fruits!
+    if @age > 5 && @age < 10
+      @fruits = 100
+    elsif @age >= 10 && @age < 15
+      @fruits = 200
     else
+      @fruits = 0
     end
   end
 end
