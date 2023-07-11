14ff6b79e7bafd45a9351618e41d3086e2d0feb3
Lucas Vilela Souza <souza.vilela.lucas@gmail.com>
2023-07-11 21:49:26 +0200

    add IP to num methdo

--- 01-Ruby/01-Programming-basics/Optional-02-Ip-Address-Converter/lib/ip.rb
+++ 01-Ruby/01-Programming-basics/Optional-02-Ip-Address-Converter/lib/ip.rb
@@ -1,7 +1,10 @@
 def ip_to_num(ip_address)
   # TODO: return the number version of the `ip_address` string
+  binary_components = ip_address.split('.').map { |num| num.to_i.to_s(2).rjust(8, "0") }
+  binary_components.join.to_i(2)
 end
 
 def num_to_ip(number)
   # TODO: return the string IP address from the `number`
+  
 end
