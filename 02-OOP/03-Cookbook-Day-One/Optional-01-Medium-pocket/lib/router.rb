f130d8724c2b805a7001437330e53b0783658713
Lucas Vilela Souza <souza.vilela.lucas@gmail.com>
2023-07-12 20:31:41 +0200

    add private in router: print_menu and route(action)

--- 02-OOP/03-Cookbook-Day-One/Optional-01-Medium-pocket/lib/router.rb
+++ 02-OOP/03-Cookbook-Day-One/Optional-01-Medium-pocket/lib/router.rb
@@ -19,4 +19,27 @@ class Router
 
 	private
 
+	def print_menu
+		puts "----------------------------"
+		puts "What do you want to do next?"
+		puts "----------------------------"
+		puts "1. List posts"
+		puts "2. Save post for later"
+		puts "3. Read post"
+		puts "4. Mark post as read"
+		puts "5. Exit"
+		print "> "
+	end
+
+	def route(action)
+		case action
+    when 1 then @controller.index
+    when 2 then @controller.create
+    when 3 then @controller.show
+    when 4 then @controller.mark_as_read
+    when 5 then @running = false
+    else
+      puts "Try again"
+    end
+	end
 end
