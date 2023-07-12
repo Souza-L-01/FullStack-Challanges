a53554ac487d728ba3cd1ad32a0e793f16f7eaac
Lucas Vilela Souza <souza.vilela.lucas@gmail.com>
2023-07-12 18:14:23 +0200

    correct indentation and add private: lead_csv and save

--- 02-OOP/03-Cookbook-Day-One/Optional-01-Medium-pocket/lib/repository.rb
+++ 02-OOP/03-Cookbook-Day-One/Optional-01-Medium-pocket/lib/repository.rb
@@ -2,28 +2,49 @@ require "csv"
 require_relative "post"
 
 class Repository
-    def initialize(csv_file)
-        post
-        @csv_file = csv_file
-        load_csv
-    end
+	def initialize(csv_file)
+		post
+		@csv_file = csv_file
+		load_csv
+	end
 
-    def all
-        @posts
-    end
+	def all
+		@posts
+	end
 
-    def add(post)
-        @posts << post
-    end
+	def add(post)
+		@posts << post
+		save
+	end
 
-    def find(index)
-        @posts[index]
-    end
+	def find(index)
+		@posts[index]
+	end
 
-    def mark_as_read(index)
-        @posts[index].mark_as_read!
-    end
+	def mark_as_read(index)
+		@posts[index].mark_as_read!
+		save
+	end
 
-    private
+  private
 
+	def load_csv
+		CSV.foreach(@csv_file) do |row|
+			@posts << Post.new(
+				path: row[0],
+				title: row[1],
+				content: row[2],
+				author: row[3],
+				read: row[4]
+			)
+		end
+	end
+
+	def save
+		CSV.open(@csv_file, "wb") do |csv|
+			@posts.each do |post|
+				csv << [post.path, post.title, post.content, post.author, post.read?]
+			end
+		end
+	end
 end
