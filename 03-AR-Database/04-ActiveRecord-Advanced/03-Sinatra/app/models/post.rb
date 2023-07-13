87b51062dfa2fbfb1e1c7e73bf02d5f56c4adb92
Lucas Vilela Souza <souza.vilela.lucas@gmail.com>
2023-07-13 19:41:01 +0200

    correct Sinatra: first change in model (scope)

--- 03-AR-Database/04-ActiveRecord-Advanced/03-Sinatra/app/models/post.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/03-Sinatra/app/models/post.rb
@@ -4,4 +4,6 @@ class Post < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 5 }, uniqueness: { case_sensitive: false }
   validates :url, presence: true, format: { with: /\Ahttps?:\/\/.*\z/ }
   validates :user, presence: true
+  
+  scope :by_most_popular, -> { order(votes: :desc) }
 end
