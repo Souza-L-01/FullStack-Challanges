8db99c4b94085a62637152f5673e9457dc265813
Lucas Vilela Souza <souza.vilela.lucas@gmail.com>
2023-07-13 20:49:39 +0200

    delete extra file

--- 03-AR-Database/04-ActiveRecord-Advanced/03-Sinatra/_03-AR-Database_04-ActiveRecord-Advanced_03-Sinatra/app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-  has_many :posts
-
-  validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, format: {
-    with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-  }
-end
