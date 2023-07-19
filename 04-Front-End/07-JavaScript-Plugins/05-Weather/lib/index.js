89e6a76c4fb4379f8387e3ca184f8ec957051768
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-07-19 17:16:48 +0200

    define index.js

--- 04-Front-End/07-JavaScript-Plugins/05-Weather/lib/index.js
+++ 04-Front-End/07-JavaScript-Plugins/05-Weather/lib/index.js
@@ -1,1 +1,8 @@
 // TODO Set up Stimulus here
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+
+window.Stimulus = Application.start();
+const context = require.context("./controllers", true, /\.js$/)
+Stimulus.load(definitionsFromContext(context))
