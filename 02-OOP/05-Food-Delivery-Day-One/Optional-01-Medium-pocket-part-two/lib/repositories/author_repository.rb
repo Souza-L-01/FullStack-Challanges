5277f3e8bbe489a8f850ae281b1444173994650f
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-07-22 22:55:46 +0200

    method add (author)

--- 02-OOP/05-Food-Delivery-Day-One/Optional-01-Medium-pocket-part-two/lib/repositories/author_repository.rb
+++ 02-OOP/05-Food-Delivery-Day-One/Optional-01-Medium-pocket-part-two/lib/repositories/author_repository.rb
@@ -12,4 +12,13 @@ class AuthorRepository
   def find(id)
     @authors.find { |author| author.id == id }
   end
+
+  def add(author)
+    author.id = @next_id
+    @authors << author
+    save
+    @next_id += 1
+    return author
+  end
+
 end
