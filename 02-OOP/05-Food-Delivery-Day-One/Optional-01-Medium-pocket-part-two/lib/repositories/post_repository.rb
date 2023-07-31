aa9e1e320d8f507e6afac56bbdfe627a1703ace3
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-07-31 21:22:05 +0200

    add Class PostRepo

--- 02-OOP/05-Food-Delivery-Day-One/Optional-01-Medium-pocket-part-two/lib/repositories/post_repository.rb
+++ 02-OOP/05-Food-Delivery-Day-One/Optional-01-Medium-pocket-part-two/lib/repositories/post_repository.rb
@@ -1,3 +1,12 @@
 require "csv"
 require_relative "../models/post"
 
+class PostRepository
+  def initialize(csv_file, author_repo)
+    @posts = []
+    @csv_file = csv_file
+    @author_repo = author_repo
+    @next_id = 1
+    load_csv
+  end
+end
