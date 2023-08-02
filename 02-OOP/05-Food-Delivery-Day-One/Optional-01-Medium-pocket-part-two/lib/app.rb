f4d687fb93ef27f6ca2a13f62989488be2e59432
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-02 18:53:46 +0200

    add var repo and controller

--- 02-OOP/05-Food-Delivery-Day-One/Optional-01-Medium-pocket-part-two/lib/app.rb
+++ 02-OOP/05-Food-Delivery-Day-One/Optional-01-Medium-pocket-part-two/lib/app.rb
@@ -4,4 +4,9 @@ require_relative "controllers/posts_controller"
 require_relative "controllers/authors_controller"
 require_relative "router"
 
-
+author_repo = AuthorRepository.new(File.join(__dir__, 'data/authors.csv'))
+post_repo = PostRepository.new(File.join(__dir__, 'data/posts.csv'), author_repo)
+posts_controller = PostsController.new(post_repo, author_repo)
+authors_controller = AuthorsController.new(author_repo)
+router = Router.new(posts_controller, authors_controller)
+router.run
