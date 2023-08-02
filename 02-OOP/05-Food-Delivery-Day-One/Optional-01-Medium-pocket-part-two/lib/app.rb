bbcb36a7b0541f8b8854abfc60640a87547efdf7
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-02 18:53:25 +0200

    add require relatives

---
+++ 02-OOP/05-Food-Delivery-Day-One/Optional-01-Medium-pocket-part-two/lib/app.rb
@@ -0,0 +1,7 @@
+require_relative "repositories/author_repository"
+require_relative "repositories/post_repository"
+require_relative "controllers/posts_controller"
+require_relative "controllers/authors_controller"
+require_relative "router"
+
+
