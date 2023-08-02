38b11446f146e409b9ea02e4c28b5d41a6cb82e6
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-02 18:57:04 +0200

    start router initialize

--- 02-OOP/05-Food-Delivery-Day-One/Optional-01-Medium-pocket-part-two/lib/router.rb
+++ 02-OOP/05-Food-Delivery-Day-One/Optional-01-Medium-pocket-part-two/lib/router.rb
@@ -1,10 +1,11 @@
-# rubocop:disable Metrics/MethodLength
 class Router
   def initialize(posts_controller, authors_controller)
     @posts_controller = posts_controller
-    @authors_controller = authors_controller
+    @authors_controller= authors_controller
     @running = true
   end
 
- end
-# rubocop:enable Metrics/MethodLength
+  def
+  end
+
+end
