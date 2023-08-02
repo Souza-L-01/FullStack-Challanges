c0adc095a618e8e11c0ec8c7b505dc997b776640
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-02 19:02:40 +0200

    add method print_menu

--- 02-OOP/05-Food-Delivery-Day-One/Optional-01-Medium-pocket-part-two/lib/router.rb
+++ 02-OOP/05-Food-Delivery-Day-One/Optional-01-Medium-pocket-part-two/lib/router.rb
@@ -20,4 +20,20 @@ class Router
   
   private
 
+  def print_menu
+    puts "-------------------------"
+    puts "What do you want to do next?"
+    puts "-------------------------"
+    puts "1. List posts"
+    puts "2. Save post for later"
+    puts "3. Read post"
+    puts "4. Mark post as read"
+    puts "5. List authors"
+    puts "6. List author's posts"
+    puts "7. See auhtor info"
+    puts "8. Exit"
+  end
+
+  def route(action)
+
 end
