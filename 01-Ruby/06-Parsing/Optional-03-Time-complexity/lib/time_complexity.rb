4f1980cf5b6d75b073462b7e7001e9023ef91713
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-07 20:32:10 +0200

    step 1 - method find_book_enhanced

--- 01-Ruby/06-Parsing/Optional-03-Time-complexity/lib/time_complexity.rb
+++ 01-Ruby/06-Parsing/Optional-03-Time-complexity/lib/time_complexity.rb
@@ -7,3 +7,12 @@ def find_book(books, book_to_find)
   return nil
 end
 
+# Solution with indexes
+# rubocop:disable Metrics/MethodLength
+def find_book_enhanced(books, book_to_find)
+  # TODO: return index of book_to_find in the array of books
+  # Use binary search routine
+  from = 0
+  to = books.length - 1
+ 
+end
