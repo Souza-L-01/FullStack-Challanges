a40283a88f4970e76768e0a6b51f8f1a80348b2e
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-07 20:31:18 +0200

    add method find_book

--- 01-Ruby/06-Parsing/Optional-03-Time-complexity/lib/time_complexity.rb
+++ 01-Ruby/06-Parsing/Optional-03-Time-complexity/lib/time_complexity.rb
@@ -1,9 +1,9 @@
 def find_book(books, book_to_find)
-  # TODO: return index of `book_to_find` in the array of `books`
-  # Use `each_with_index`
+  # TODO: return index of book_to_find in the array of books
+  # Use each_with_index
+  books.each_with_index do |book, index|
+    return index if book == book_to_find
+  end
+  return nil
 end
 
-def find_book_enhanced(books, book_to_find)
-  # TODO: return index of `book_to_find` in the array of `books`
-  # Use binary search
-end
